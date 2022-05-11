65da2214df8c0122c1955a124c87a390117d4acd
Oni Barolli <obarolli@gmail.com>
2022-05-11 00:27:54 -0700

    Added config helper object

---
+++ config/tradebot_config.py
@@ -0,0 +1,33 @@
+import os
+from __future__ import annotations
+
+from utils import fileutils
+from utils.utils import singleton, check_singleton
+
+__this_dir = os.path.dirname(__file__)
+settings_file = fileutils.join(__this_dir, "settings.yml")
+
+
+@singleton
+class TradeBotConfigs:
+
+    @staticmethod
+    def get_config() -> TradeBotConfigs:
+        if TradeBotConfigs.__instance:
+            return TradeBotConfigs.__instance
+        else:
+            self = TradeBotConfigs()
+            self.__data = fileutils.read(settings_file, fileutils.yml_to_dict)
+            TradeBotConfigs.__instance = self
+            return self
+
+
+    @check_singleton
+    def get(self, key:str) -> str:
+        return self.__data.get(key)
+
+
+    @check_singleton
+    def update(self, key:str, value:str) -> None:
+        entry = {key:value}
+        self.__data.update(entry)
