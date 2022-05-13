a7286e718061ccfb554f5eacd68fb1baee5bee9a
Oni Barolli <obarolli@gmail.com>
2022-05-13 08:11:53 -0700

    Added documentation to get_price_history

--- bot/tradebot.py
+++ bot/tradebot.py
@@ -5,6 +5,7 @@ from utils.httputils import BadResponseException
 import requests
 from urllib.parse import urlencode
 
+
 class TradeBot:
 
     def __init__(self, configs:tradebot_config.TradeBotConfigs):
@@ -12,15 +13,36 @@ class TradeBot:
         self.__base_url = "https://api.tdameritrade.com/v1/"
 
     
-    def get_price_history(self, ticket:str, periodType:str, period:int, frequencyType:str, frequency:str):
+    def get_price_history(self, ticker:str, periodType:str, period:int, frequencyType:str, frequency:str):
+        '''
+        'ticker' is the stock ticker to be queried.
+        'periodType' is the period to show. Valid values
+        include 'day', 'month', 'year', or 'ytd'.
+        'period' is the number of periods to show.
+        'frequencyType' is the frequency units to use. Valid
+        values will depend on the 'periodType'. Valid values for each 
+        periodType are shown; the default value is marked with an asterisk.
+
+            day: minute*
+            month: daily, weekly*
+            year: daily, weekly, monthly*
+            ytd: daily, weekly*
+
+        'frequency' is the number of frequencyType to include in each candle.
+        Valid frequencies by frequencyType (defaults marked with an asterisk):
+
+            minute: 1*, 5, 10, 15, 30
+            daily: 1*
+            weekly: 1*
+            monthly: 1*
+        '''
         apikey = self.__configs.get("consumer_key")
         access_token = self.__configs.get("access_token")
         query_string = urlencode({"apikey": apikey, "periodType": periodType, "period": period, "frequencyType": frequencyType, "frequency": frequency})
-        url = f"{self.__base_url}marketdata/{ticket}/pricehistory?{query_string}"
+        url = f"{self.__base_url}marketdata/{ticker}/pricehistory?{query_string}"
         headers = {
             "Authorization": f"Bearer {access_token}"
         }
-        
         res = requests.get(url, headers=headers)
 
         if not res.ok:
@@ -29,7 +51,7 @@ class TradeBot:
         return res.json()
         
 
-    def access_token_request(self):
+    def post_access_token(self):
         grant_type = "refresh_token"
         refresh_token = self.__configs.get("refresh_token")
         client_id = self.__configs.get("consumer_key")
