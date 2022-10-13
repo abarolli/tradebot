915add73f9d249c5f74fd5863f7c59218b46a15b
abarolli <obarolli@gmail.com>
2022-10-13 11:14:50 -0700

    Need auth to get account info

--- tradebot/account/td_account.py
+++ tradebot/account/td_account.py
@@ -1,13 +1,14 @@
-from tradebot.requests import TradebotRequests
+from tradebot.requests import TradebotRequests, assert_ok_response
 from tradebot.configs import TradebotConfigs
+
 
 def get_account_info(configs:TradebotConfigs, fields:str=None):
     acct_number = configs["account_number"]
     url = f"https://api.tdameritrade.com/v1/accounts/{acct_number}"
     url_params = {"fields": fields} if fields else ''
     requests = TradebotRequests(configs)
-    res = requests.get(url, params=url_params, auth=False)
-    assert res.ok
+    res = requests.get(url, params=url_params)
+    assert_ok_response(res)
 
     return res.json()
 
